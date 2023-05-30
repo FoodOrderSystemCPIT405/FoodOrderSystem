0c4d6851d623453f71384bebfda10b7fee7c51fe
Essam Alsaud <essamx123@gmail.com>
2023-05-30 15:52:23 +0300

    js page

---
+++ foodItem.js
@@ -0,0 +1,16 @@
+const foodItem = [
+    {
+        id: 1,
+        name: 'Cheese Pizza',
+        category: 'Pizza',
+        price: 10,
+        img: 'images/burger.jpg',
+    },
+    {
+        id: 2,
+        name: 'Chicken Burger',
+        category: 'Burger',
+        price: 15,
+        img: 'images/burger5.jpg',
+    }
+]
